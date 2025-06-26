aab90c87c998e79d8891609ed3d69ab1f007cf77
Muhammad Muneeb <muneebn048@gmail.com>
2025-06-26 18:21:27 +0500

    added digg.java

---
+++ digg.java
@@ -0,0 +1,8 @@
+public class Diff {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int difference = a - b;
+        System.out.println("The difference between " + a + " and " + b + " is: " + difference);
+    }
+}
